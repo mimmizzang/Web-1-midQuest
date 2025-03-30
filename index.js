526f31488e52191e312ac430bf4647bc5fa54d16
mimmizzang <fpahs0920@gamil.com>
2025-03-30 17:36:22 +0900

    3rd commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// slide
+
 let currentIndex = 2;
 const slides = document.querySelectorAll(".slide_box");
 const totalSlides = slides.length;
@@ -24,3 +26,35 @@ function nextSlide() {
 }
 
 updateSlides(); // 초기화
+
+// slide2
+
+// gsap
+
+// scroll
+
+const scrollBadge = document.getElementById("badge_box");
+const scrollTopBtn = document.getElementById("scroll_top");
+
+window.onscroll = function () {
+  const scrollY = document.documentElement.scrollTop;
+
+  if (scrollY > 500) {
+    scrollTopBtn.style.display = "inline-block";
+    scrollBadge.classList.add("hidden");
+
+    setTimeout(() => {
+      if (scrollBadge.classList.contains("hidden")) {
+        scrollBadge.style.display = "none";
+      }
+    }, 1000);
+  } else {
+    scrollTopBtn.style.display = "none";
+    scrollBadge.classList.remove("hidden");
+    scrollBadge.style.display = "block";
+  }
+};
+
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
